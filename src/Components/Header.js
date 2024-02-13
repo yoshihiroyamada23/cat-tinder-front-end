9e8fbc787da4f2c8d52ffaea749a82330c3c6c33
RobertSarsoza <sarsoza@gmail.com>
2024-02-13 11:24:25 -0800

    made changes based off comments

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,23 +11,23 @@ const Header = () => {
                 alt="Home Logo"
                 className="HomeLogo"
             />
-        <Nav>
-            <RouterNavLink to="/" className="nav-link"> Home </RouterNavLink>
-            
-            <RouterNavLink to="/catindex" className="nav-link">
-                Meet All the Cats
-            </RouterNavLink>
-            <RouterNavLink to="/catnew" className="nav-link">
-                Add Your Cat
-            </RouterNavLink>
-            <a
-                target="blank"
-                href="https://tinder.com/"
-                className="nav-link"
-            >
-                Find your Purrfect match!
-            </a>
-        </Nav>
+            <Nav>
+                <RouterNavLink to="/" className="nav-link"> Home </RouterNavLink>
+
+                <RouterNavLink to="/catindex" className="nav-link">
+                    Meet All the Cats
+                </RouterNavLink>
+                <RouterNavLink to="/catnew" className="nav-link">
+                    Add Your Cat
+                </RouterNavLink>
+                <a
+                    target="blank"
+                    href="https://tinder.com/"
+                    className="nav-link"
+                >
+                    Find your Purrfect match!
+                </a>
+            </Nav>
         </div>
     )
 }
