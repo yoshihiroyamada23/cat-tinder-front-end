6d7ebb16826a9b5a641d46bece5a6ee9a61f82b9
Yoshihiro Yamada <yoshihiro.yamada1995@gmail.com>
2024-02-14 14:44:23 -0800

    added new url in fetch calls

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ const App = () => {
   }, [])
 
   const readCat = () => {
-    fetch("http://localhost:3000/cats")
+    fetch("https://cat-tinder-wro1.onrender.com/cats")
       .then((response) => response.json(response))
       .then((payload) => setCats(payload))
       .catch((errors) => console.log("cat read errors", errors))
   }
   const createCat = (cat) => {
-    fetch("http://localhost:3000/cats", {
+    fetch("https://cat-tinder-wro1.onrender.com/cats", {
       body: JSON.stringify(cat),
       headers: {
         "Content-Type": "application/json"
